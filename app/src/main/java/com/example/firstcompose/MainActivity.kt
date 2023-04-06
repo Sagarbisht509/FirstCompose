9399b81527c26f597a938d95b305eda8ea334c9c
sagar <sagarbisht509@gmail.com>
2023-04-07 02:21:17 +0530

    TextField

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -7,7 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firstcompose.ui.theme.FirstComposeTheme
@@ -16,10 +19,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            userInput()
         }
     }
 }
 
+@Composable
+fun userInput() {
+    val state = remember{mutableStateOf("")}
+    TextField(
+        value = state.value,
+        onValueChange = {
+            state.value = it
+        },
+        label = {
+            Text(text = "Enter Message")
+        }
+    )
+}
+
+/*
 @Composable
 fun SayHello(name: String = "Not Set") {
     Text(text = name)
@@ -30,3 +49,5 @@ fun SayHello(name: String = "Not Set") {
 fun PreviewFunction() {
     SayHello("pass by value")
 }
+
+*/
